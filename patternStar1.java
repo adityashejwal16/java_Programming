f0776b6437b2318e526eee93101186bee829c9f0
Aditya Shejwal <adityashejwal016@gmail.com>
2026-02-21 23:45:13 +0530

    Add files via upload

---
+++ patternStar1.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class Pattern
+{
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+    
+    if(iRow != iCol)
+        {
+            System.out.println("Invalid Input");
+            System.out.println("Row number column number should be same");
+            return;
+        }
+        for(i=1; i<=iRow; i++)
+        {
+              
+            for(j=1; i >= j; j++)
+
+           {
+             System.out.print("*\t");
+           }
+
+            System.out.println();
+        } 
+    }
+}
+
+class patternStar1
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter Number Of rows");
+        iValue1=sobj.nextInt();
+
+        System.out.println("Enter Number Of columns");
+        iValue2=sobj.nextInt();
+
+        Pattern pobj=new Pattern();
+        pobj.Pattern(iValue1,iValue2);    
+
+    }
+}
