e9cfc4ef8df7a6d58b02ea62ae512fd19751017d
Aditya Shejwal <adityashejwal016@gmail.com>
2026-04-06 23:05:41 +0530

    Add files via upload

--- ReverseGeneric.java
+++ ReverseGeneric.java
@@ -1,32 +1,30 @@
 class ReverseGeneric
 {
-
-    public static <T> void Reverse(T Arr[], int iSize)
+    public static <T> void Reverse(T arr[])
     {
-        T temp;
-        for(int i = 0; i < iSize / 2; i++)
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end)
         {
-          temp = Arr[i];
-          Arr[i] = Arr[iSize -1 -i];
-          Arr[iSize -1 -i ] = temp;
+            T temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
         }
     }
 
     public static void main(String A[])
     {
-        Integer arr[] = {10, 20, 30, 10, 30, 40, 10, 40, 10};
+        Integer arr[] = {10,20,30,40,50};
 
-        for(int j = 0; j < arr.length; j++)
+        Reverse(arr);
+
+        for(Integer no : arr)
         {
-            System.out.print(arr[j] + " ");
+            System.out.print(no+" ");
         }
-        System.out.println();
-        Reverse(arr, arr.length);
-
-        for(int j = 0; j < arr.length; j++)
-        {
-              System.out.print(arr[j] + " ");
-        }
-
     }
 }
