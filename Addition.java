9ced1426a70f916bd8691fdbea91726f9760f129
Aditya Shejwal <adityashejwal016@gmail.com>
2026-09-22 21:40:23 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,13 @@
+
+class Addition
+{
+    public static void main(String A[]) 
+    {
+        int No1 = 10,  No2 = 52;
+
+        int iRet = No1 + No2;
+
+        System.out.println("Addition is " + iRet);
+    }
+
+}
